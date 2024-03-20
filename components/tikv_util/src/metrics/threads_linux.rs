703c07696d3037115a0c46ba868add59318a4bf7
suuyaoo <suuyaoo@gmail.com>
2024-03-20 21:17:20 +0800

    adapt load test run may fail

--- components/tikv_util/src/metrics/threads_linux.rs
+++ components/tikv_util/src/metrics/threads_linux.rs
@@ -1,22 +1,21 @@
 // Copyright 2017 TiKV Project Authors. Licensed under Apache-2.0.
 
-use std::fs;
 use std::io::{Error, ErrorKind, Result};
 use std::sync::Mutex;
 use std::time::Duration;
 
 use crate::collections::HashMap;
-use libc::{self, pid_t};
 use prometheus::core::{Collector, Desc};
 use prometheus::{self, proto, CounterVec, IntCounterVec, IntGaugeVec, Opts};
 
+use crate::sys::thread::{self, Pid};
 use crate::time::Instant;
 
 use procinfo::pid;
 
 /// Monitors threads of the current process.
 pub fn monitor_threads<S: Into<String>>(namespace: S) -> Result<()> {
-    let pid = unsafe { libc::getpid() };
+    let pid = thread::process_id();
     let tc = ThreadsCollector::new(pid, namespace);
     prometheus::register(Box::new(tc)).map_err(|e| to_io_err(format!("{:?}", e)))
 }
@@ -32,14 +31,14 @@ struct Metrics {
 /// A collector to collect threads metrics, including CPU usage
 /// and threads state.
 struct ThreadsCollector {
-    pid: pid_t,
+    pid: Pid,
     descs: Vec<Desc>,
     metrics: Mutex<Metrics>,
     tid_retriever: Mutex<TidRetriever>,
 }
 
 impl ThreadsCollector {
-    fn new<S: Into<String>>(pid: pid_t, namespace: S) -> ThreadsCollector {
+    fn new<S: Into<String>>(pid: Pid, namespace: S) -> ThreadsCollector {
         let mut descs: Vec<Desc> = vec![];
         let ns = namespace.into();
         let cpu_totals = CounterVec::new(
@@ -117,9 +116,9 @@ impl Collector for ThreadsCollector {
         let tids = tid_retriever.get_tids();
         for tid in tids {
             let tid = *tid;
-            if let Ok(stat) = pid::stat_task(self.pid, tid) {
+            if let Ok(stat) = thread::full_thread_stat(self.pid, tid) {
                 // Threads CPU time.
-                let total = cpu_total(&stat);
+                let total = thread::linux::cpu_total(&stat);
                 // sanitize thread name before push metrics.
                 let name = sanitize_thread_name(tid, &stat.command);
                 let cpu_total = metrics
@@ -200,38 +199,6 @@ impl Collector for ThreadsCollector {
     }
 }
 
-/// Gets thread ids of the given process id.
-/// WARN: Don't call this function frequently. Otherwise there will be a lot of memory fragments.
-pub fn get_thread_ids(pid: pid_t) -> Result<Vec<pid_t>> {
-    let mut tids: Vec<i32> = fs::read_dir(format!("/proc/{}/task", pid))?
-        .filter_map(|task| {
-            let file_name = match task {
-                Ok(t) => t.file_name(),
-                Err(e) => {
-                    error!("read task failed"; "pid" => pid, "err" => ?e);
-                    return None;
-                }
-            };
-
-            match file_name.to_str() {
-                Some(tid) => match tid.parse() {
-                    Ok(tid) => Some(tid),
-                    Err(e) => {
-                        error!("read task failed"; "pid" => pid, "err" => ?e);
-                        None
-                    }
-                },
-                None => {
-                    error!("read task failed"; "pid" => pid);
-                    None
-                }
-            }
-        })
-        .collect();
-    tids.sort();
-    Ok(tids)
-}
-
 /// Sanitizes the thread name. Keeps `a-zA-Z0-9_:`, replaces `-` and ` ` with `_`, and drops the others.
 ///
 /// Examples:
@@ -244,7 +211,7 @@ pub fn get_thread_ids(pid: pid_t) -> Result<Vec<pid_t>> {
 /// assert_eq!(sanitize_thread_name(1, "@123"), "123");
 /// assert_eq!(sanitize_thread_name(1, "@@@@"), "1");
 /// ```
-fn sanitize_thread_name(tid: pid_t, raw: &str) -> String {
+fn sanitize_thread_name(tid: Pid, raw: &str) -> String {
     let mut name = String::with_capacity(raw.len());
     // sanitize thread name.
     for c in raw.chars() {
@@ -281,21 +248,8 @@ fn state_to_str(state: &pid::State) -> &str {
     }
 }
 
-pub fn cpu_total(state: &pid::Stat) -> f64 {
-    (state.utime + state.stime) as f64 / *CLK_TCK
-}
-
 fn to_io_err(s: String) -> Error {
     Error::new(ErrorKind::Other, s)
-}
-
-lazy_static! {
-    // getconf CLK_TCK
-    static ref CLK_TCK: f64 = {
-        unsafe {
-            libc::sysconf(libc::_SC_CLK_TCK) as f64
-        }
-    };
 }
 
 #[inline]
@@ -355,7 +309,7 @@ impl ThreadMetrics {
 
 /// Use to collect cpu usages and disk I/O rates
 pub struct ThreadInfoStatistics {
-    pid: pid_t,
+    pid: Pid,
     last_instant: Instant,
     tid_names: HashMap<i32, String>,
     tid_retriever: TidRetriever,
@@ -365,7 +319,7 @@ pub struct ThreadInfoStatistics {
 
 impl ThreadInfoStatistics {
     pub fn new() -> Self {
-        let pid = unsafe { libc::getpid() };
+        let pid = thread::process_id();
 
         let mut thread_stats = Self {
             pid,
@@ -394,11 +348,11 @@ impl ThreadInfoStatistics {
         for tid in tids {
             let tid = *tid;
 
-            if let Ok(stat) = pid::stat_task(self.pid, tid) {
+            if let Ok(stat) = thread::full_thread_stat(self.pid, tid) {
                 let name = get_name(&stat.command);
                 self.tid_names.entry(tid).or_insert(name);
 
-                let cpu_time = cpu_total(&stat) * 100.0;
+                let cpu_time = thread::linux::cpu_total(&stat) * 100.0;
                 update_metric(
                     &mut self.metrics_total.cpu_times,
                     &mut self.metrics_rate.cpu_times,
@@ -450,27 +404,30 @@ const TID_MAX_UPDATE_INTERVAL: Duration = Duration::from_secs(10 * 60);
 
 /// A helper that buffers the thread id list internally.
 struct TidRetriever {
-    pid: pid_t,
+    pid: Pid,
     tid_buffer: Vec<i32>,
     tid_buffer_last_update: Instant,
     tid_buffer_update_interval: Duration,
 }
 
 impl TidRetriever {
-    pub fn new(pid: pid_t) -> Self {
+    pub fn new(pid: Pid) -> Self {
+        let mut tid_buffer: Vec<_> = thread::thread_ids(pid).unwrap();
+        tid_buffer.sort_unstable();
         Self {
             pid,
-            tid_buffer: get_thread_ids(pid).unwrap(),
+            tid_buffer,
             tid_buffer_last_update: Instant::now(),
             tid_buffer_update_interval: TID_MIN_UPDATE_INTERVAL,
         }
     }
 
-    pub fn get_tids(&mut self) -> &[pid_t] {
+    pub fn get_tids(&mut self) -> &[Pid] {
         // Update the tid list according to tid_buffer_update_interval.
         // If tid is not changed, update the tid list less frequently.
         if self.tid_buffer_last_update.saturating_elapsed() >= self.tid_buffer_update_interval {
-            let new_tid_buffer = get_thread_ids(self.pid).unwrap();
+            let mut new_tid_buffer: Vec<_> = thread::thread_ids(self.pid).unwrap();
+            new_tid_buffer.sort_unstable();
             if new_tid_buffer == self.tid_buffer {
                 self.tid_buffer_update_interval *= 2;
                 if self.tid_buffer_update_interval > TID_MAX_UPDATE_INTERVAL {
@@ -492,7 +449,7 @@ mod tests {
     use std::env::temp_dir;
     use std::io::Write;
     use std::time::Duration;
-    use std::{fs, sync, thread};
+    use std::{fs, sync};
 
     use libc;
 
@@ -503,7 +460,7 @@ mod tests {
         let name = "testthreadio";
         let (tx, rx) = sync::mpsc::channel();
         let (tx1, rx1) = sync::mpsc::channel();
-        let h = thread::Builder::new()
+        let h = std::thread::Builder::new()
             .name(name.to_owned())
             .spawn(move || {
                 // Make `io::write_bytes` > 0
@@ -522,13 +479,13 @@ mod tests {
         rx1.recv().unwrap();
 
         let page_size = unsafe { libc::sysconf(libc::_SC_PAGE_SIZE) as usize };
-        let pid = unsafe { libc::getpid() };
-        let tids = get_thread_ids(pid).unwrap();
+        let pid = thread::process_id();
+        let tids: Vec<_> = thread::thread_ids(pid).unwrap();
         assert!(tids.len() >= 2);
 
         tids.iter()
             .find(|t| {
-                pid::stat_task(pid, **t)
+                thread::full_thread_stat(pid, **t)
                     .map(|stat| stat.command == name)
                     .unwrap_or(false)
             })
@@ -552,7 +509,7 @@ mod tests {
     ) -> (sync::mpsc::Sender<()>, sync::mpsc::Receiver<()>) {
         let (tx, rx) = sync::mpsc::channel();
         let (tx1, rx1) = sync::mpsc::channel();
-        thread::Builder::new()
+        std::thread::Builder::new()
             .name(str1.to_owned())
             .spawn(move || {
                 tx1.send(()).unwrap();
@@ -594,10 +551,10 @@ mod tests {
         let mut thread_info = ThreadInfoStatistics::new();
 
         let page_size = unsafe { libc::sysconf(libc::_SC_PAGE_SIZE) as u64 };
-        let pid = unsafe { libc::getpid() };
-        let tids = get_thread_ids(pid).unwrap();
+        let pid = thread::process_id();
+        let tids: Vec<_> = thread::thread_ids(pid).unwrap();
         for tid in tids {
-            if let Ok(stat) = pid::stat_task(pid, tid) {
+            if let Ok(stat) = thread::full_thread_stat(pid, tid) {
                 if stat.command.starts_with(s1) {
                     rx1.recv().unwrap();
                     thread_info.record();
@@ -638,7 +595,7 @@ mod tests {
     ) -> (sync::mpsc::Sender<()>, sync::mpsc::Receiver<()>) {
         let (tx, rx) = sync::mpsc::channel();
         let (tx1, rx1) = sync::mpsc::channel();
-        thread::Builder::new()
+        std::thread::Builder::new()
             .name(name)
             .spawn(move || {
                 tx1.send(()).unwrap();
@@ -668,10 +625,10 @@ mod tests {
 
         let mut thread_info = ThreadInfoStatistics::new();
 
-        let pid = unsafe { libc::getpid() };
-        let tids = get_thread_ids(pid).unwrap();
+        let pid = thread::process_id();
+        let tids: Vec<_> = thread::thread_ids(pid).unwrap();
         for tid in tids {
-            if let Ok(stat) = pid::stat_task(pid, tid) {
+            if let Ok(stat) = thread::full_thread_stat(pid, tid) {
                 if stat.command.starts_with(tn) {
                     rx.recv().unwrap();
                     thread_info.record();
@@ -729,7 +686,7 @@ mod tests {
 
     #[test]
     fn test_smoke() {
-        let pid = unsafe { libc::getpid() };
+        let pid = thread::process_id();
         let tc = ThreadsCollector::new(pid, "smoke");
         tc.collect();
         tc.desc();
--- src/server/load_statistics/mod.rs
+++ src/server/load_statistics/mod.rs
@@ -1,53 +1,55 @@
 // Copyright 2018 TiKV Project Authors. Licensed under Apache-2.0.
 
+use parking_lot::Mutex;
+use std::cell::RefCell;
 use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use tikv_util::sys::thread::{self, Pid};
+use tikv_util::collections::HashMap;
+
+thread_local! {
+    static CURRENT_LOAD: RefCell<Option<Arc<AtomicUsize>>> = RefCell::new(None);
+}
 
 /// A load metric for all threads.
 pub struct ThreadLoad {
-    term: AtomicUsize,
-    load: AtomicUsize,
+    stats: Mutex<HashMap<Pid, Arc<AtomicUsize>>>,
     threshold: usize,
+    total_load: AtomicUsize,
 }
 
 impl ThreadLoad {
     /// Constructs a new `ThreadLoad` with the specified threshold.
     pub fn with_threshold(threshold: usize) -> Self {
         ThreadLoad {
-            term: AtomicUsize::new(0),
-            load: AtomicUsize::new(0),
+            stats: Mutex::default(),
             threshold,
+            total_load: AtomicUsize::new(0),
         }
     }
 
-    #[allow(dead_code)]
-    pub fn get_threshold(&self) -> usize {
-        // read-only
-        self.threshold
-    }
-
-    /// Returns true if the current load exceeds its threshold.
-    #[allow(dead_code)]
+    /// Check if current thread is in heavy load.
     pub fn in_heavy_load(&self) -> bool {
-        self.load.load(Ordering::Relaxed) > self.threshold
-    }
-
-    /// Increases when updating `load`.
-    #[allow(dead_code)]
-    pub fn term(&self) -> usize {
-        self.term.load(Ordering::Relaxed)
+        CURRENT_LOAD.with(|l| {
+            let mut l = l.borrow_mut();
+            if l.is_none() {
+                let mut loads = self.stats.lock();
+                *l = Some(loads.entry(thread::thread_id()).or_default().clone());
+            }
+            l.as_ref().unwrap().load(Ordering::Relaxed) >= self.threshold
+        })
     }
 
     /// Gets the current load. For example, 200 means the threads consuming 200% of the CPU resources.
-    #[allow(dead_code)]
     pub fn load(&self) -> usize {
-        self.load.load(Ordering::Relaxed)
+        self.total_load.load(Ordering::Relaxed)
     }
 }
 
 #[cfg(target_os = "linux")]
 mod linux;
 #[cfg(target_os = "linux")]
-pub use self::linux::*;
+pub use self::linux::ThreadLoadStatistics;
 
 #[cfg(not(target_os = "linux"))]
 mod other_os {
