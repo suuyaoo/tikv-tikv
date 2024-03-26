fc9c1b00a69253f271890bf58fe6a80454470c7b
suuyaoo <suuyaoo@gmail.com>
2024-03-26 17:16:45 +0800

    patch rocksdb

--- components/engine_rocks/src/rocks_metrics.rs
+++ components/engine_rocks/src/rocks_metrics.rs
@@ -690,11 +690,6 @@ pub fn flush_engine_properties(engine: &DB, name: &str, shared_block_cache: bool
                 .set(block_cache_usage as i64);
         }
 
-        let blob_cache_usage = engine.get_blob_cache_usage_cf(handle);
-        STORE_ENGINE_BLOB_CACHE_USAGE_GAUGE_VEC
-            .with_label_values(&[name, cf])
-            .set(blob_cache_usage as i64);
-
         // TODO: find a better place to record these metrics.
         // Refer: https://github.com/facebook/rocksdb/wiki/Memory-usage-in-RocksDB
         // For index and filter blocks memory
@@ -793,11 +788,6 @@ lazy_static! {
     pub static ref STORE_ENGINE_BLOCK_CACHE_USAGE_GAUGE_VEC: IntGaugeVec = register_int_gauge_vec!(
         "tikv_engine_block_cache_size_bytes",
         "Usage of each column families' block cache",
-        &["db", "cf"]
-    ).unwrap();
-    pub static ref STORE_ENGINE_BLOB_CACHE_USAGE_GAUGE_VEC: IntGaugeVec = register_int_gauge_vec!(
-        "tikv_engine_blob_cache_size_bytes",
-        "Usage of each column families' blob cache",
         &["db", "cf"]
     ).unwrap();
     pub static ref STORE_ENGINE_MEMORY_GAUGE_VEC: IntGaugeVec = register_int_gauge_vec!(
